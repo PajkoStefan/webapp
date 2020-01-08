1ad60acd749e1e8eb456158586f47625693c68e5
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-01-08 23:45:05 +0100

    Add publisher class

---
+++ src/main/java/com/stefan/spf/webapp/model/Publisher.java
@@ -0,0 +1,17 @@
+package com.stefan.spf.webapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Publisher {
+
+    private String name;
+    private String address;
+
+}
