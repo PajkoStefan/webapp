61b6430d08e192bd14e4cdb93a4aa9174c6af866
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-01-08 23:59:59 +0100

    Add equals, hash and toString

--- src/main/java/com/stefan/spf/webapp/model/Publisher.java
+++ src/main/java/com/stefan/spf/webapp/model/Publisher.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,6 +27,31 @@ public class Publisher {
     public Publisher(String name, String address){
         this.name = name;
         this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 
 }
