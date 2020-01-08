6d863fe6841872d9c261b5381610bfb0649f025b
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-01-08 23:02:28 +0100

    Add entities to database

--- src/main/java/com/stefan/spf/webapp/model/Author.java
+++ src/main/java/com/stefan/spf/webapp/model/Author.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,11 +13,16 @@ import java.util.Set;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity(name = "Author")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
     public Author(String firstName, String lastName){
--- src/main/java/com/stefan/spf/webapp/model/Book.java
+++ src/main/java/com/stefan/spf/webapp/model/Book.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,12 +13,19 @@ import java.util.Set;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity(name = "Book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book(String title, String isbn, String publisher){
