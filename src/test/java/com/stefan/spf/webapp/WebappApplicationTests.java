7bc200945a43a8b1e08a37488392cba15a3d7eef
PajkoStefan <52094853+PajkoStefan@users.noreply.github.com>
2020-01-08 22:31:43 +0100

    project initialization

---
+++ src/test/java/com/stefan/spf/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefan.spf.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
